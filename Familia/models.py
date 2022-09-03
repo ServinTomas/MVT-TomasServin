2df62866e5e840ceb8ad56466dc2bf2889893c87
ServinTomas <servintomas98@gmail.com>
2022-09-03 15:23:57 -0300

    Nuevas actualizaciones

--- Familia/models.py
+++ Familia/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Familiares(models.Model):
+    nombre = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField()
+    peso_kg = models.IntegerField()
+
+    
+    
+
+
+class Datos(models.Model):
+    profesion = models.CharField(max_length=128)
+    hobbys = models.CharField(max_length=128)
