53fc84ecad797a3dc0d9af4eddc62a40ec4e31e7
ServinTomas <servintomas98@gmail.com>
2022-09-08 11:02:05 -0300

    Nuevos cambios

--- Familia/views.py
+++ Familia/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from Familia.models import Familiares
 # Create your views here.
+
+def listar(request):
+    queryset = Familiares.objects.all()
+    diccionario = {"Familia" : queryset}
+    plantilla = loader.get_template("Familia_list.html")
+    documento_html = plantilla.render(diccionario)
+    return HttpResponse(documento_html)
+
